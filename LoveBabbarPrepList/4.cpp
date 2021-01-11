97703268ee5c35b8ded68c18cdb66ed60eb91e0b
arora.sir <a.mohit1998@gmail.com>
2021-01-12 05:06:15 +0530

    sort012

---
+++ LoveBabbarPrepList/4.cpp
@@ -0,0 +1,12 @@
+#include <climits>
+#include <bits/stdc++.h>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int arr[9] = {0, 1, 2, 2, 1, 2, 0, 1, 0};
+
+    // count 0, 1 and 2 and put in array
+    // using sort function
+}
