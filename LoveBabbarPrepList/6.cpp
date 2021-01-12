ed5c01fcb33771eb84087af873293aacacfa4f91
arora.sir <a.mohit1998@gmail.com>
2021-01-12 15:38:35 +0530

    Union & Intersection of array

---
+++ LoveBabbarPrepList/6.cpp
@@ -0,0 +1,17 @@
+#include <climits>
+#include <bits/stdc++.h>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    //Union
+    //unordered_map <int,int> (take count of all unique elements)
+    //compare both 1st element, print which is smaller → increment that pointer (both if same) → In end print remaining elements of larger array
+
+    //Intersection
+    //set<int> hash → Store 1st array and iterate to 2nd array, if present then print (hs.find(arr2[i]) != hs.end())
+
+    int a[4] = {1, 2, 3, 4};
+    int b[5] = {4, 3, 5, 6, 7};
+}
