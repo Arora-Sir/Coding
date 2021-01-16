c7bc85b44dd89efcbfeb4f4ade3df0d9f2ff7450
arora.sir <a.mohit1998@gmail.com>
2021-01-17 04:57:18 +0530

    Tapping Water in between buildings, O(n),O(1)

--- LoveBabbarPrepList/29.cpp
+++ LoveBabbarPrepList/29.cpp
@@ -1,34 +1,42 @@
-//Tapping Water
+//Tapping Water in between buildings
 
+//App 1 : O(n^2), O(1) → Calculate minimum from both sides of element to get maximum reachable height → sum += min(left, right)-a[i]
+//App 2 : O(n),O(n) → Store in 2 arrays left[n] (calculating max from left for every element),right[n] (from right) → sum += min(left[i], right[i])-a[i]
+//App 3 : O(n),O(1) → Two pointer approach from left_most and right_most (done here)
 
 // { Driver Code Starts
-#include<bits/stdc++.h>
-
+#include <bits/stdc++.h>
 using namespace std;
-
-
- // } Driver Code Ends
+// } Driver Code Ends
 
 // function to find the trapped water in between buildings
 // arr: input array
 // n: size of array
-int trappingWater(int arr[], int n){
-    
-    int sum=0;
-    int end=n-1;
-    int start=0;
-    int startmax =0;
-    int endmax =0;
-    
-    while(start<end){
-        if(arr[start] < arr[end]){
-            if(arr[start] >= startmax) startmax = arr[start];
-            else sum+= startmax-arr[start];
+int trappingWater(int arr[], int n)
+{
+
+    int sum = 0;
+    int end = n - 1;
+    int start = 0;
+    int startmax = 0; //left max height
+    int endmax = 0;   //right max height
+
+    while (start < end)
+    {
+        if (arr[start] < arr[end])
+        {
+            if (arr[start] >= startmax)
+                startmax = arr[start];
+            else
+                sum += startmax - arr[start];
             start++;
         }
-        else{
-            if(arr[end] >= endmax) endmax = arr[end];
-            else sum+= endmax-arr[end];
+        else
+        {
+            if (arr[end] >= endmax)
+                endmax = arr[end];
+            else
+                sum += endmax - arr[end];
             end--;
         }
     }
@@ -37,29 +45,31 @@ int trappingWater(int arr[], int n){
 
 // { Driver Code Starts.
 
-int main(){
-    
+int main()
+{
+
     int t;
     //testcases
     cin >> t;
-    
-    while(t--){
+
+    while (t--)
+    {
         int n;
-        
+
         //size of array
         cin >> n;
-        
+
         int a[n];
-        
+
         //adding elements to the array
-        for(int i =0;i<n;i++){
-            cin >> a[i];            
+        for (int i = 0; i < n; i++)
+        {
+            cin >> a[i];
         }
-        
+
         //calling trappingWater() function
         cout << trappingWater(a, n) << endl;
-        
     }
-    
+
     return 0;
-}  // } Driver Code Ends
+} // } Driver Code Ends
