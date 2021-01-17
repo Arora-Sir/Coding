670aec03f580c72c6e333fb09f1efd22916bb092
arora.sir <a.mohit1998@gmail.com>
2021-01-17 20:04:39 +0530

    35

---
+++ LoveBabbarPrepList/35.cpp
@@ -0,0 +1,49 @@
+//Median of Array
+
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+// } Driver Code Ends
+
+class Solution
+{
+public:
+public:
+    int find_median(vector<int> v)
+    {
+        sort(v.begin(), v.end());
+        int size = v.size();
+        if (size % 2 == 0)
+        {
+            int x = (size / 2) - 1;
+            int y = x + 1;
+            int z = (v[x] + v[y]) / 2;
+            return z;
+        }
+        else
+        {
+            int x = size / 2;
+            return v[x];
+        }
+    }
+};
+
+// { Driver Code Starts.
+int main()
+{
+    int T;
+    cin >> T;
+    while (T--)
+    {
+        int n;
+        cin >> n;
+        vector<int> v(n);
+        for (int i = 0; i < n; i++)
+            cin >> v[i];
+        Solution ob;
+        int ans = ob.find_median(v);
+        cout << ans << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
