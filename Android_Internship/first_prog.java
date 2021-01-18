04dad24d1e866dcdc60e21eab0c372b2bd7397d3
arora.sir <a.mohit1998@gmail.com>
2021-01-19 02:55:50 +0530

    Java 1st prog learn

---
+++ Android_Internship/first_prog.java
@@ -0,0 +1,24 @@
+public class first_prog {
+    public static void main(String[] mohit) {
+        System.out.println("hello");
+    }
+}
+/*
+ * Q1: Why main() method is declared as public? to call method from jvm from
+ * anywhere so main() is declared a public
+ * 
+ * Q2: Why main method is declared as static? Ans - To call main() method by JVM
+ * (Java Virtual Machine) without creating any object JVM -> output kaam karne
+ * ka kaam jvm karega..main nahi
+ * 
+ * Q3: Why main() declared as void? Ans: It doesn't return any thing so main()
+ * is declared as void
+ * 
+ * Q4: String[] arg:- it'll take command line argument It will take any type of
+ * data args:- is a
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
