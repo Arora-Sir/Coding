5f33b96484ae72a9ef2c63919745315fcea7cbaf
arora.sir <a.mohit1998@gmail.com>
2021-01-24 18:28:56 +0530

    Java 1st prog

---
+++ Android_Internship/HelloWorld.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class HelloWorld {
+    public static void main(String[] args) { // main + tab
+        System.out.println("Hello"); // system think it of as a file you can code to out put whatever you are
+                                     // printing
+        // out - > output
+        // print -> print the content
+
+        System.out.print("Hello\n"); // syso + tab
+        System.out.print("Hello");
+        System.out.print("Hello\n");
+
+        int a;
+        a = 10;
+        System.out.println(a); // create -> initialize -> then print use
+
+        int isValid; // camel case
+        boolean num = true; // only true or false
+        System.out.println(num);
+    }
+}
---
+++ Android_Internship/tri.java
@@ -0,0 +1,6 @@
+public class tri {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+
+}
