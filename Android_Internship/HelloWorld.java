b8d703f6059df1ce086485c2849a9e72390ed182
arora.sir <a.mohit1998@gmail.com>
2021-01-24 19:12:57 +0530

    .

--- Android_Internship/HelloWorld.java
+++ Android_Internship/HelloWorld.java
@@ -18,5 +18,26 @@ public class HelloWorld {
         int isValid; // camel case
         boolean num = true; // only true or false
         System.out.println(num);
+
+        int b;
+        // ! Necessary thing -> Scanner
+        Scanner s = new Scanner(System.in); // S capital + java.util.scanner added
+        b = s.nextInt(); // take input from user (fetch the number)
+
+        // if I enter string instead of int here, it'll give me error
+        System.out.println(b);
+        int c = s.nextInt(); // take input from user
+
+        int d = b + c;
+        System.out.println(d);
+
+        String str = s.next();
+        // it'll not print if I have spaces in between, If I want entire line then
+        String stro = s.nextLine(); // for entire line (with spaces)
+
+        // ? what about char?? Indirect root
+        String st = s.next(); // first take line or a word
+        char ch = st.charAt(0); // take input a single character which is at first
+
     }
 }
