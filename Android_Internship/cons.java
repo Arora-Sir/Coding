8133183baf092fb2d79c3090f50841a69fcab6f2
arora.sir <a.mohit1998@gmail.com>
2021-01-27 19:04:21 +0530

    "this" keyword use

--- Android_Internship/cons.java
+++ Android_Internship/cons.java
@@ -4,16 +4,19 @@ public class cons {
 
     cons() {
         this(2, 3, 4);
+        System.out.println("wihtout");
         System.out.println("hi");
     }
 
     cons(int a, int b) {
+        System.out.println("with 2");
         int c = a + b;
         System.out.println(c);
     }
 
     cons(int a, int b, int c) {
         this(23, 5);
+        System.out.println("with 3");
         int d = a * c;
         System.out.println(d);
     }
