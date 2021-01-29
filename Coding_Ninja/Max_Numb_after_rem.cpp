a2f494f5e6884de63515054fb69857019845ce6a
arora.sir <a.mohit1998@gmail.com>
2021-01-29 23:43:28 +0530

    Coding Ninja (Test 2)

---
+++ Coding_Ninja/Max_Numb_after_rem.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int num = 1324;
+    int numSave = num;
+    int mini = INT_MAX;
+    int newNum = 0, mul = 1;
+
+    while (num > 0)
+    {
+        int temp = num % 10;
+        if (temp < mini)
+            mini = temp;
+        num /= 10;
+    }
+    while (numSave > 0)
+    {
+        int temp = numSave % 10;
+        if (temp != mini)
+        {
+            newNum += temp * mul;
+            mul *= 10;
+        }
+        numSave /= 10;
+    }
+    cout << newNum << endl;
+}
