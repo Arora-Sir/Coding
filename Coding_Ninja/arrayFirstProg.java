09c903e287b2a9cd3b638854f9fd286b510c468e
arora.sir <a.mohit1998@gmail.com>
2021-02-03 02:58:36 +0530

    Array 1st Prog

---
+++ Coding_Ninja/arrayFirstProg.java
@@ -0,0 +1,29 @@
+public class arrayFirstProg {
+    public static void main(String[] args) {
+        boolean[] a = new boolean[5];
+        System.out.println(a[0]);
+
+        int[] ar = new int[5];
+        System.out.println(ar[0]);
+
+        long[] aar = new long[5];
+        System.out.println(aar[0]);
+
+        char[] arr = new char[5];
+        System.out.println(arr[0]);
+
+        double[] arrr = new double[5];
+        System.out.println(arrr[0]);
+
+        float[] arrrr = new float[5];
+        System.out.println(arrrr[1]);
+
+        short[] arrrrr = new short[5];
+        System.out.println(arrrrr[0]);
+
+        int trying[] = new int[5];
+        trying[1] = 10;
+        trying = new int[2];
+        System.out.println(trying[1]);
+    }
+}
