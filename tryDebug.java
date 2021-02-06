a9134dadf4f75bb198d72d010c2bcd63da64d43e
arora.sir <a.mohit1998@gmail.com>
2021-02-06 23:28:41 +0530

    Trying debugging in java vscode

---
+++ tryDebug.java
@@ -0,0 +1,8 @@
+public class tryDebug {
+
+    public static void main(String[] args) {
+        String s1 = new String("hey");
+        String s2 = new String("hey");
+        System.out.println(s1 == s2);
+    }
+}
