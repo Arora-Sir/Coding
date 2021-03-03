b44e79dfd37618392b57bf944e74cb915ea2796c
arora.sir <a.mohit1998@gmail.com>
2021-03-03 22:42:03 +0530

    Coding Ninja (Lecture 14 : Recursion 1)

---
+++ Coding_Ninja/noOfDigits.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class noOfDigits {
+    public static void main(String args[]) {
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        System.out.println(count(n));
+    }
+
+    public static int count(int n) {
+        if (n == 0) {
+            return 0;
+        }
+        int smallAns = count(n / 10);
+        return smallAns + 1;
+    }
+
+}
