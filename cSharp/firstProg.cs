147aa7f7d4846360ed1bc66a3ad705278e534405
arora.sir <a.mohit1998@gmail.com>
2021-07-08 23:56:56 +0530

    Added C# code for the first time

---
+++ cSharp/firstProg.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace cSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title="AroraJi";
+            // Console.WindowHeight=1;
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+            Console.ForegroundColor=ConsoleColor.Green;
+            Console.WriteLine("Yeah I'm green baby");
+            Console.ForegroundColor=ConsoleColor.White;
+            Console.WriteLine("Yeah");
+            Console.WriteLine("Yeah");
+
+            Console.ReadKey();
+        }
+    }
+}
